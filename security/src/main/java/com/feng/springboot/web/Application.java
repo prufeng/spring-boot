77f02128ee006b3870467e38ed49dc6dc17b3342
PanRufeng <prufeng@sina.com>
2018-04-21 23:28:17 +0800

    Spring Boot example: web and security

---
+++ security/src/main/java/com/feng/springboot/web/Application.java
@@ -0,0 +1,14 @@
+package com.feng.springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by PanRufeng on 2018/4/21.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ web/src/main/java/com/feng/springboot/web/GreetingController.java
@@ -0,0 +1,18 @@
+package com.feng.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by PanRufeng on 2018/4/21.
+ */
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
